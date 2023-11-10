c08dfd39557bd8d13b8580f93ddf5fefd96d9cad
Spy <Spyngunga@gmail.com>
2023-11-10 07:29:45 +0300

    Completed Assigment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ColorList() {
-  const colors = [
+  const color = [
     "firebrick",
     "rebeccapurple",
     "salmon",
@@ -9,15 +9,15 @@ function ColorList() {
     "hotpink",
   ];
 
+  const colorElements = color.map((color) => {
+    return <li style={{ color: color }}>{color}</li>;
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {/*display the array of <li> elements here!*/}
+        {colorElements}
       </ol>
     </div>
   );
